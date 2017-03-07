de70d477d108723125b2531601ed3d05e7eb6de1
comsci_kanyasiri <s5752410007@sau.ac.th>
2017-03-07 16:11:09 +0700

    Study DB 5752410007

---
+++ indec.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home page</title>
+<hr>
+</head>
+<body>
+<center>
+<h1> Home Page </h1>
+<hr>
+<form action="singinprocess.php" method="post">
+	User name :
+<input type="text" name="uname">
+<br><br>
+	Password : <input type="password" name="upassword" >
+	<br><br>
+	<input type="submit" value="Sign In">
+	<input type="reset" value="Cencel">
+</form>	
+<br>
+<a href="registermember.php">ลงทะเบีจยน</a>
+</center>
+
+
+	
+</body>
+</html>
